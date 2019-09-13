3e0b6f95e3c77696b1af95b976d1e9b36f5158e7
Emil Martini <emil.martini@hotmail.com>
2019-09-13 14:16:44 +0200

    add hello world again

--- DungeonCrawler/Program.cs
+++ DungeonCrawler/Program.cs
@@ -11,6 +11,7 @@ namespace DungeonCrawler
         static void Main(string[] args)
         {
             Console.WriteLine("Hello World!");
+            Console.WriteLine("Hello world again!");
         }
     }
 }
