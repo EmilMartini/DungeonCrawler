efc66b07ecb0cf7efc218ddc4657a1dd1af2ecc2
emil martini <emil.martini@hotmail.com>
2019-09-14 12:41:47 +0200

    add tile and room class. add constructor method for tile class. add init method for room class. add visualize method for room class.

--- DungeonCrawler/Player.cs
+++ DungeonCrawler/Player.cs
@@ -8,5 +8,6 @@ namespace DungeonCrawler
 {
     public class Player
     {
+
     }
 }
--- DungeonCrawler/Tile.cs
+++ DungeonCrawler/Tile.cs
@@ -1,9 +1,24 @@
-﻿namespace DungeonCrawler
+﻿using System;
+
+namespace DungeonCrawler
 {
     internal class Tile
     {
-        public Tile()
+        public enum TileType { Floor, Key, Door}
+        public TileType tileType;
+
+        public int TileHorizontal;
+        public int TileVertical;
+
+        public string Display;
+
+        public Tile(TileType assignedTileType, int horizontal, int vertical, string display)
         {
+            tileType = assignedTileType;
+            TileHorizontal = horizontal;
+            TileVertical = vertical;
+            Display = display;
         }
+
     }
 }
