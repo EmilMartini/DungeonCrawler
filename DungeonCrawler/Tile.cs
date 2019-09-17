59e6a82a87d669ac6869f138d7abe4b37733aa14
Emil Martini <emil.martini@hotmail.com>
2019-09-17 14:26:27 +0200

    resolved conflict

--- DungeonCrawler/Tile.cs
+++ DungeonCrawler/Tile.cs
@@ -6,10 +6,6 @@ namespace DungeonCrawler
     {
         public TileType TileType { get; set; }
         public string Render { get; set; }
-<<<<<<< HEAD
-
         public Point Point { get; set; }
-=======
->>>>>>> 49bf545717270968d6808039e02f1ab4c9d60e92
     }
 }
