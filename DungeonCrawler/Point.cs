455ff5ebbebd15a190d6361185b65a7c77c7bbab
John Andersson <john.andersson1995@gmail.com>
2019-09-17 13:48:09 +0200

    add Point struct

---
+++ DungeonCrawler/Point.cs
@@ -0,0 +1,14 @@
+﻿
+namespace DungeonCrawler
+{
+    public struct Point
+    {
+        public Point(int x, int y)
+        {
+            X = x;
+            Y = y;
+        }
+        public readonly int X;
+        public readonly int Y;
+    }
+}
