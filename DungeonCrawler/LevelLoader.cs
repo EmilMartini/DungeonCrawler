328aa42ed36a44d71c5b6a57dc151fdba8d9b2ab
John Andersson <john.andersson1995@gmail.com>
2019-09-19 15:24:33 +0200

    add LevelLayout and LevelLoader class rename Map to Level

---
+++ DungeonCrawler/LevelLoader.cs
@@ -0,0 +1,12 @@
+﻿namespace DungeonCrawler
+{
+    class LevelLoader
+    {
+        private Level[] levels;
+
+        public LevelLoader(Level[] levels)
+        {
+            this.levels = levels;
+        }
+    }
+}
