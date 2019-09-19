384d3abbd480a6c30649f3ecd23d9dd46a49fdc8
Emil Martini <emil.martini@hotmail.com>
2019-09-19 21:51:28 +0200

    change size for level1

--- DungeonCrawler/LevelLayout.cs
+++ DungeonCrawler/LevelLayout.cs
@@ -4,7 +4,7 @@
     {
         public Level[] Levels = new Level[3];
         
-        Level level1 = new Level(new Size(16, 16), new Point(1,1));
+        Level level1 = new Level(new Size(25, 25), new Point(1,1));
         Level level2 = new Level(new Size(18, 18), new Point(17, 17));
         Level level3 = new Level(new Size(24, 14), new Point(1, 1));
 
