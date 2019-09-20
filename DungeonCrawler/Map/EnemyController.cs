a689388361fbbaa0d95bf6d75fc74bf3bc8906a7
emil martini <emil.martini@hotmail.com>
2019-09-20 15:45:22 +0200

    add monster and monster.move method in progress

---
+++ DungeonCrawler/Map/EnemyController.cs
@@ -0,0 +1,41 @@
+﻿using System;
+
+namespace DungeonCrawler
+{
+    internal class EnemyController
+    {
+        Random rnd = new Random();
+        public Point currentEnemyPosition;
+        private readonly Level level;
+        private readonly MapController mapController;
+
+        public EnemyController(Level level, MapController mapController)
+        {
+            this.level = level ?? throw new ArgumentNullException(nameof(level));
+            this.mapController = mapController ?? throw new ArgumentNullException(nameof(mapController));
+        }
+
+        public void Move()
+        {
+            for (int i = 0; i < level.Enemies.Length; i++)
+            {
+                int row = 0, column = 0;
+
+                while (row == 0 && column == 0)
+                {
+                    row = rnd.Next(-1, 2);
+                    column = rnd.Next(-1, 2);
+                }
+
+                currentEnemyPosition = new Point(currentEnemyPosition.row + row, currentEnemyPosition.column + column);
+
+                if (level.InitialLayout[currentEnemyPosition.row, currentEnemyPosition.column].TileType != TileType.Wall)
+                {
+                    mapController.UpdateMonsterPosition(level.Enemies[i], currentEnemyPosition);
+                }
+            }     
+        }
+
+        
+    }
+}
