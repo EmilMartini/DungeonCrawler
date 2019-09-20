470a56e6dc51e7b6d6a383d30aeba1713ce547c8
emil martini <emil.martini@hotmail.com>
2019-09-20 15:53:21 +0200

    fixed bug in EnemyController

--- DungeonCrawler/Map/EnemyController.cs
+++ DungeonCrawler/Map/EnemyController.cs
@@ -6,6 +6,7 @@ namespace DungeonCrawler
     {
         Random rnd = new Random();
         public Point currentEnemyPosition;
+        public Point nextEnemyPosition;
         private readonly Level level;
         private readonly MapController mapController;
 
@@ -26,12 +27,12 @@ namespace DungeonCrawler
                     row = rnd.Next(-1, 2);
                     column = rnd.Next(-1, 2);
                 }
+                currentEnemyPosition = new Point(level.Enemies[i].Position.row, level.Enemies[i].Position.column);
+                nextEnemyPosition = new Point(currentEnemyPosition.row + row, currentEnemyPosition.column + column);
 
-                currentEnemyPosition = new Point(currentEnemyPosition.row + row, currentEnemyPosition.column + column);
-
-                if (level.InitialLayout[currentEnemyPosition.row, currentEnemyPosition.column].TileType != TileType.Wall)
+                if (level.InitialLayout[nextEnemyPosition.row, nextEnemyPosition.column].TileType != TileType.Wall)
                 {
-                    mapController.UpdateMonsterPosition(level.Enemies[i], currentEnemyPosition);
+                    mapController.UpdateMonsterPosition(level.Enemies[i], nextEnemyPosition);
                 }
             }     
         }
