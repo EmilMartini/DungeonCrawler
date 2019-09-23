3a0dcdf7e395c1707b08bd21ad77558b697ba539
John Andersson <john.andersson1995@gmail.com>
2019-09-23 14:56:18 +0200

    make doors work

--- DungeonCrawler/Scripts/PlayerController.cs
+++ DungeonCrawler/Scripts/PlayerController.cs
@@ -44,10 +44,14 @@ namespace DungeonCrawler
                                              currentPosition.column + directionColumn);
             if (level.InitialLayout[targetPosition.row, targetPosition.column] is IInteractable interactable)
             {
-                 bool interactionSucceded = interactable.Interact();
+                bool interactionSucceded = interactable.Interact();
                 if (interactionSucceded)
                 {
                     level.InitialLayout[targetPosition.row, targetPosition.column] = new Floor();
+                }
+                else
+                {
+                    return;
                 }
             }
             if (level.InitialLayout[targetPosition.row, targetPosition.column].TileType != TileType.Wall)
