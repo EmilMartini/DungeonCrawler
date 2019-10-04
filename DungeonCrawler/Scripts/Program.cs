d2432f64cd63b7031dd3a5cd78fe714eda343dbe
John Andersson <john.andersson1995@gmail.com>
2019-10-04 20:19:44 +0200

    change to object initializer in program

--- DungeonCrawler/Scripts/Program.cs
+++ DungeonCrawler/Scripts/Program.cs
@@ -18,39 +18,41 @@ namespace DungeonCrawler
 
         private static void InitGame()
         {
-            var gameLevels = LevelCreator.GetLevels(); 
+            var gameLevels = LevelCreator.GetLevels();
             player = new Player();
-            gameplayManager = new GameplayManager(gameLevels, player);
-            gameplayManager.CurrentState = GameplayState.InitializeLevel;
+            gameplayManager = new GameplayManager(gameLevels, player)
+            {
+                CurrentState = GameplayState.InitializeLevel
+            };
         }
 
         private static void WelcomeScreen()
-            {
-                Console.WriteLine();
-                Console.WriteLine("\tWelcome to a dungeon crawler you'll never forget.");
-                Console.ForegroundColor = ConsoleColor.Green;
-                Console.Write("\t@ ");
-                Console.ForegroundColor = ConsoleColor.White;
-                Console.Write("<- This is you");
-                Console.Write("! \n\tCollect keys to advance through the locked doors. \n\tBut be wary, there are ");
-                Console.ForegroundColor = ConsoleColor.Red;
-                Console.Write("monsters ");
-                Console.ForegroundColor = ConsoleColor.White;
-                Console.Write("lurking these halls....\n\n\n\t\t\t\tGood luck!\n");
-                Console.WriteLine("\t\t\tPress any key to start...");
-                Console.Write("\n\n\n\n\n\t|Legend\n\t|\n\t|Keys: K\n\t|Door: D\n\t|Player: @\n\t|Monsters: M");
-                Console.WriteLine("\n\n\n\n\n\n\n\n\tMade by:");
-                Console.WriteLine("\tJohn Andersson & Emil Martini");
-                Console.ReadKey(true);
-                Console.Clear();
-            }
+        {
+            Console.WriteLine();
+            Console.WriteLine("\tWelcome to a dungeon crawler you'll never forget.");
+            Console.ForegroundColor = ConsoleColor.Green;
+            Console.Write("\t@ ");
+            Console.ForegroundColor = ConsoleColor.White;
+            Console.Write("<- This is you");
+            Console.Write("! \n\tCollect keys to advance through the locked doors. \n\tBut be wary, there are ");
+            Console.ForegroundColor = ConsoleColor.Red;
+            Console.Write("monsters ");
+            Console.ForegroundColor = ConsoleColor.White;
+            Console.Write("lurking these halls....\n\n\n\t\t\t\tGood luck!\n");
+            Console.WriteLine("\t\t\tPress any key to start...");
+            Console.Write("\n\n\n\n\n\t|Legend\n\t|\n\t|Keys: K\n\t|Door: D\n\t|Player: @\n\t|Monsters: M");
+            Console.WriteLine("\n\n\n\n\n\n\n\n\tMade by:");
+            Console.WriteLine("\tJohn Andersson & Emil Martini");
+            Console.ReadKey(true);
+            Console.Clear();
+        }
 
         private static void SetConsoleProperties()
-            {
-                var consoleWindowSize = new Size(77, 36);
-                Console.CursorVisible = false;
-                Console.SetWindowSize((int)consoleWindowSize.Width, (int)consoleWindowSize.Height);
-                Console.SetBufferSize((int)consoleWindowSize.Width + 1, (int)consoleWindowSize.Height + 1);
-            }
+        {
+            var consoleWindowSize = new Size(77, 36);
+            Console.CursorVisible = false;
+            Console.SetWindowSize((int) consoleWindowSize.Width, (int) consoleWindowSize.Height);
+            Console.SetBufferSize((int) consoleWindowSize.Width + 1, (int) consoleWindowSize.Height + 1);
+        }
     }
 }
